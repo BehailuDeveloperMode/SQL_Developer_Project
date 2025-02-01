0d3798f9e07204082ecd096f55129ac5074d7f97
Behailu Tessema <behailu@developermode.dev>
2025-02-01 01:52:38 -0800

    Update SSIS_Transformation.sql
    this is updated the transaction

--- SSIS_Transformation.sql
+++ SSIS_Transformation.sql
@@ -36,22 +36,22 @@ SELECT * FROM @UnpivotTable;
 --===========
 SELECT *
 ,SUM(Salary)over(PARTITION BY Month_Name ORDER BY Month_Name) AS totalsalary_bymonth  --Total Salry for each particular month.
-    ,CASE 
-        WHEN Month_Name = 'Jan' THEN 1
-          WHEN Month_Name = 'Feb' THEN 2
-           WHEN Month_Name = 'Mar' THEN 3
-            WHEN Month_Name = 'Apr' THEN 4
-             WHEN Month_Name = 'May' THEN 5
-              WHEN Month_Name = 'Jun' THEN 6
-               WHEN Month_Name = 'Jul' THEN 7
-                WHEN Month_Name = 'Aug' THEN 8
-                 WHEN Month_Name = 'Sep' THEN 9
-                  WHEN Month_Name = 'Oct' THEN 10
-                   WHEN Month_Name = 'Nov' THEN 11
-                    WHEN Month_Name = 'Dec' THEN 12
-                     ELSE NULL -- Handle invalid input if necessary
-             END AS Month_Number
-       FROM --Final Output from SubQuery
+,CASE 
+WHEN Month_Name = 'Jan' THEN 1
+    WHEN Month_Name = 'Feb' THEN 2
+    WHEN Month_Name = 'Mar' THEN 3
+    WHEN Month_Name = 'Apr' THEN 4
+        WHEN Month_Name = 'May' THEN 5
+        WHEN Month_Name = 'Jun' THEN 6
+        WHEN Month_Name = 'Jul' THEN 7
+        WHEN Month_Name = 'Aug' THEN 8
+            WHEN Month_Name = 'Sep' THEN 9
+            WHEN Month_Name = 'Oct' THEN 10
+            WHEN Month_Name = 'Nov' THEN 11
+            WHEN Month_Name = 'Dec' THEN 12
+                ELSE NULL -- Handle invalid input if necessary
+        END AS Month_Number
+FROM --Final Output from SubQuery
 (
 SELECT * 
 FROM @UnpivotTable) AS Row_Data  -- Origional declared Data.
@@ -74,78 +74,78 @@ SELECT SUBSTRING(
 				
 --==================----------   Incremental data Load in SSIS using LookUP
 -- Create a Audit_log Table
-	 IF NOT EXISTS
-		 (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Audit_log]') AND type IN (N'U'))
-	BEGIN
-			CREATE TABLE Audit_log
-				 (
-				 Id INT IDENTITY,
-				 PakageName VARCHAR(200),
-				 TableName VARCHAR(200),
-				 RecordsInserted INT,
-				 RecordsUpdated INT,
-				 Dated DATETIME
-				 ) ON [PRIMARY]
-			  PRINT 'The table is Created'
-	END
-		ELSE
-			  PRINT 'The table is Exists'
-	 GO
+IF NOT EXISTS
+	(SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Audit_log]') AND type IN (N'U'))
+BEGIN
+	CREATE TABLE Audit_log
+			(
+			Id INT IDENTITY,
+			PakageName VARCHAR(200),
+			TableName VARCHAR(200),
+			RecordsInserted INT,
+			RecordsUpdated INT,
+			Dated DATETIME
+			) ON [PRIMARY]
+		PRINT 'The table is Created'
+END
+ELSE
+		PRINT 'The table is Exists'
+GO
 --====================  Create an Emails Table
-		IF NOT EXISTS
-		   (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Emails]') AND TYPE IN(N'U'))
-	BEGIN
-			CREATE TABLE Emails
-				 (
-				 Id INT NULL,
-				 First_Name VARCHAR(50) NULL,
-				 Last_Name VARCHAR(50) NULL,
-				 Email VARCHAR(50) NULL,
-				 Gender VARCHAR(50) NULL			
-				 ) ON [PRIMARY]
-			PRINT 'The table is Created'
-	END
-		ELSE
-		   PRINT 'The table is Exists'
-	 GO
+IF NOT EXISTS
+	(SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Emails]') AND TYPE IN(N'U'))
+BEGIN
+	CREATE TABLE Emails
+			(
+			Id INT NULL,
+			First_Name VARCHAR(50) NULL,
+			Last_Name VARCHAR(50) NULL,
+			Email VARCHAR(50) NULL,
+			Gender VARCHAR(50) NULL			
+			) ON [PRIMARY]
+	PRINT 'The table is Created'
+END
+ELSE
+	PRINT 'The table is Exists'
+GO
 --============================  Create  ZZ_Emaile_Updated  Table   
-		IF NOT EXISTS
-		   (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ZZ_Emaile_Updated]') AND TYPE IN(N'U'))
-	BEGIN
-			CREATE TABLE ZZ_Emaile_Updated
-				 (
-				 Id INT NULL,
-				 First_Name VARCHAR(50) NULL,
-				 Last_Name VARCHAR(50) NULL,
-				 Email VARCHAR(50) NULL,
-				 Gender VARCHAR(50) NULL			
-				 ) ON [PRIMARY]
-			PRINT 'The table is Created'
-	END
-		ELSE
-		   PRINT 'The table is Exists'
-	 GO
+IF NOT EXISTS
+	(SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ZZ_Emaile_Updated]') AND TYPE IN(N'U'))
+BEGIN
+	CREATE TABLE ZZ_Emaile_Updated
+			(
+			Id INT NULL,
+			First_Name VARCHAR(50) NULL,
+			Last_Name VARCHAR(50) NULL,
+			Email VARCHAR(50) NULL,
+			Gender VARCHAR(50) NULL			
+			) ON [PRIMARY]
+	PRINT 'The table is Created'
+END
+ELSE
+	PRINT 'The table is Exists'
+GO
 --===================================
-		IF NOT EXISTS
-		   (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Archive]') AND TYPE IN(N'U'))
-	BEGIN
-			CREATE TABLE Archive
-				 (
-				 Id INT IDENTITY (100,1),
-				 Email_Id INT NOT NULL,
-				 First_Name VARCHAR(50) NULL,
-				 Last_Name VARCHAR(50) NULL,
-				 Email VARCHAR(50) NULL,
-				 Gender VARCHAR(50) NULL,
-				 Dataed DATETIME DEFAULT GETDATE(),
-				 Activity VARCHAR(25) NOT NULL,
-				 ) ON [PRIMARY]
-			PRINT 'The table is Created'
-	END
-		ELSE
-		   PRINT 'The table is Exists'
-	 GO
---====================================
+IF NOT EXISTS
+	(SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Archive]') AND TYPE IN(N'U'))
+BEGIN
+	CREATE TABLE Archive
+			(
+			Id INT IDENTITY (100,1),
+			Email_Id INT NOT NULL,
+			First_Name VARCHAR(50) NULL,
+			Last_Name VARCHAR(50) NULL,
+			Email VARCHAR(50) NULL,
+			Gender VARCHAR(50) NULL,
+			Dataed DATETIME DEFAULT GETDATE(),
+			Activity VARCHAR(25) NOT NULL,
+			) ON [PRIMARY]
+	PRINT 'The table is Created'
+END
+ELSE
+	PRINT 'The table is Exists'
+GO
+--========================================================
 DECLARE @Updated INT
 UPDATE a
 SET a.First_Name = b.First_Name,
@@ -159,75 +159,75 @@ SET @Updated = @@ROWCOUNT
 INSERT INTO [dbo].[Audit_log]
 SELECT 'Package.dtsx','dbo.Emails', 0 , @Updated ,GETDATE() -- or
 --SELECT ? -- 0 
---      ,? -- 1
---	  ,? -- 2
---	  ,@Updated 
---	  ,GETDATE()
+--,? -- 1
+--,? -- 2
+--,@Updated 
+--,GETDATE()
 --=======================================
 GO
 CREATE OR ALTER TRIGGER tr_Archaive ON [dbo].[Emails]
 FOR INSERT,UPDATE,DELETE
 AS
 SET NOCOUNT ON
- IF EXISTS (SELECT 0 FROM deleted)
-  BEGIN
-  	  IF EXISTS (SELECT 0 FROM inserted)
- BEGIN
-    INSERT INTO [dbo].[Archive]
+IF EXISTS (SELECT 0 FROM deleted)
+BEGIN
+  	IF EXISTS (SELECT 0 FROM inserted)
+BEGIN
+INSERT INTO [dbo].[Archive]
+(
+Email_Id,
+First_Name,
+Last_Name,
+Email,
+Gender,
+[Activity]
+)
+SELECT d.Id AS Email_Id ,
+		d.First_Name,
+		d.Last_Name,
+		d.Email,
+		d.Gender,
+		'Update' AS  Activity 
+FROM deleted d
+END
+ELSE
+BEGIN
+INSERT INTO [dbo].[Archive]
+(
+Email_Id,
+First_Name,
+Last_Name,
+Email,
+Gender,
+Activity 
+)
+SELECT d.Id AS Email_Id ,
+		d.First_Name,
+		d.Last_Name,
+		d.Email ,
+		d.Gender,
+		'Delete'  AS Activity 
+FROM deleted d
+END
+END
+ELSE
+BEGIN
+INSERT INTO [dbo].[Archive]
 	(
 	Email_Id,
 	First_Name,
 	Last_Name,
 	Email,
 	Gender,
-	[Activity]
+	Activity 
 	)
-		  SELECT d.Id AS Email_Id ,
-				   d.First_Name,
-				   d.Last_Name,
-				   d.Email,
-				   d.Gender,
-				  'Update' AS  Activity 
-		   FROM deleted d
-	 END
-    ELSE
-  BEGIN
-      INSERT INTO [dbo].[Archive]
-		(
-		Email_Id,
-		First_Name,
-		Last_Name,
-		Email,
-		Gender,
-		Activity 
-		)
-		SELECT d.Id AS Email_Id ,
-			   d.First_Name,
-			   d.Last_Name,
-			   d.Email ,
-			   d.Gender,
-			  'Delete'  AS Activity 
-		FROM deleted d
-      END
-   END
-    ELSE
-  BEGIN
-  INSERT INTO [dbo].[Archive]
-		(
-		Email_Id,
-		First_Name,
-		Last_Name,
-		Email,
-		Gender,
-		Activity 
-		)
-		SELECT Ins.Id AS Email_Id ,
-			   Ins.First_Name,
-			   Ins.Last_Name,
-			   Ins.Email ,
-			   Ins.Gender,
-			  'Insert'  AS Activity 
-		FROM inserted Ins
+SELECT Ins.Id AS Email_Id ,
+		Ins.First_Name,
+		Ins.Last_Name,
+		Ins.Email ,
+		Ins.Gender,
+		'Insert'  AS Activity 
+FROM inserted Ins
 END
 --=================================
 -- To Extract a specific value from a string 
@@ -238,8 +238,10 @@ SELECT SUBSTRING(@dateString, CHARINDEX(',', @dateString) + 2, 4) AS SecondChara
 --================================
 GO
 -- To find a comma possition that occurrence on the thired possition. 
+
 DECLARE @DateString NVARCHAR(50) = 'Feb 28, 2024, 6,:26 PM';
 SELECT CHARINDEX(',', @dateString, CHARINDEX(',', @dateString) + 1) AS SecondCommaPosition;
+
 --SecondCommaPosition
 --13
 Go
@@ -255,18 +257,81 @@ SELECT @DateValue AS ExtractedDate
 --2024-02-28
 GO
 --=================================
-  -- This query helps us find the number of transactions performed within a specific 
-  --date interval. In our case, the interval is the last 7 days from today's date
+--This query helps us find the number of transactions performed within a specific 
+--date interval. In our case, the interval is the last 7 days from today's date
+--Scenario
+--Assume we have a table called @View_STGFamilyLiving that records daily transactions. 
+--The table has the following data:
 
-  SELECT COUNT(DISTINCT DATE) AS Count_Of_Transaction 
-      FROM vw_STG_FamilyLiving
-         WHERE DATE >=  -- BY USING SUB QUERY
-          (SELECT DISTINCT DATEADD(DAY, -7, GETDATE()) AS seven_days_ago FROM vw_STG_FamilyLiving)
-
+DECLARE @View_STGFamilyLiving TABLE 
+(
+    ID INT PRIMARY KEY,
+    Date DATE,
+    TransactionAmount DECIMAL(10,2)
+);
+INSERT INTO @View_STGFamilyLiving 
+(ID, Date, TransactionAmount)
+VALUES
+(1, '2025-01-25', 100.00),
+(2, '2025-01-26', 200.00),
+(3, '2025-01-27', 150.00),
+(4, '2025-01-28', 300.00),
+(5, '2025-01-29', 250.00),
+(6, '2025-01-30', 400.00),
+(7, '2025-01-31', 500.00),
+(8, '2025-02-01', 600.00);
   --=================
-  -- The simplest form of the query can be solved using this streamlined version.
-  -- This clearly indicates that the provided query is a simplified and more efficient alternative to the original.
+  --Now, you can execute either of these queries to count the number of distinct transaction dates in the last 7 days:
+
+SELECT COUNT(DISTINCT DATE) AS Count_Of_Transaction 
+FROM @View_STGFamilyLiving
+WHERE DATE >=  
+      (SELECT DISTINCT DATEADD(DAY, -7, GETDATE()) AS seven_days_ago FROM @View_STGFamilyLiving)
+
+--GETDATE() returns today’s date (2025-02-01).
+--DATEADD(DAY, -7, GETDATE()) subtracts 7 days, giving 2025-01-25.
+--The WHERE clause ensures we count only transactions from 2025-01-25 to 2025-02-01.
+--The COUNT(DISTINCT DATE) ensures we count unique transaction dates.
+--The simplest form of the query can be solved using this streamlined version.
+--This clearly indicates that the provided query is a simplified and more efficient alternative to the original.
   
-		SELECT COUNT(DISTINCT DATE) AS Count_Of_Transaction
-                  FROM vw_STG_FamilyLiving
-                         WHERE DATE >= DATEADD(DAY, -7, GETDATE());
+SELECT COUNT(DISTINCT DATE) AS Count_Of_Transaction
+FROM @View_STGFamilyLiving
+WHERE DATE >= DATEADD(DAY, -7, GETDATE());
+
+--The second query is more efficient and produces the same result.
+--DATEADD(DAY, -7, GETDATE()) ensures we get the last 7 days, including today.
+--COUNT(DISTINCT DATE) ensures only unique dates are counted.
+--====================================================
+--If you're getting 7 instead of 8, it's likely because of how GETDATE() works. Let's debug step by step.
+--Potential Issue: GETDATE() Returns Date & Time
+--GETDATE() returns both date and time, e.g., 2025-02-01 14:30:00.000.
+--When using DATEADD(DAY, -7, GETDATE()), it calculates **2025-01-25 14:30:00.000`.
+--If your Date column is of type DATE (without time), it only stores 2025-01-25.
+--This causes the query to exclude the earliest date (2025-01-25) because of the >= condition.
+--Solution: Use CAST(GETDATE() AS DATE)
+
+SELECT COUNT(DISTINCT Date) AS Count_Of_Transaction
+FROM @View_STGFamilyLiving
+WHERE Date >= DATEADD(DAY, -7, CAST(GETDATE() AS DATE));
+
+--Why This Works
+--CAST(GETDATE() AS DATE) removes the time part, so it becomes 2025-02-01 00:00:00.000.
+--Now, DATEADD(DAY, -7, CAST(GETDATE() AS DATE)) gives 2025-01-25 00:00:00.000, correctly including 2025-01-25.
+        
+--Alternative Debugging Query
+--Run this to check the calculated date:
+
+SELECT DATEADD(DAY, -7, GETDATE()) AS WithTime,
+DATEADD(DAY, -7, CAST(GETDATE() AS DATE)) AS WithoutTime;
+
+--If the WithTime result is causing an issue, the WithoutTime version will fix it.
+          
+--Expected Fix Output
+--Now, when you run:
+
+SELECT COUNT(DISTINCT Date) AS Count_Of_Transaction
+FROM @View_STGFamilyLiving
+WHERE Date >= DATEADD(DAY, -7, CAST(GETDATE() AS DATE));
+
+--=================================================================
